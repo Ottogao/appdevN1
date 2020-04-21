2bdab103e428306e6aecac43552b3c6f46a99044
Chao Gao <gc@shell.vamk.fi>
2020-04-21 08:37:00 +0300

    For sound functions and main.c

---
+++ main.c
@@ -0,0 +1,24 @@
+// this file will be used to test functions in screen.c
+#include <stdio.h>
+#include <unistd.h>
+#include "screen.h"
+#include "comm.h"
+#include "sound.h"
+
+int main(void){
+	Position p = getscreensize();
+	if(p.col<BARS){
+		printf("Your screen size is not wide enough to display %d bars\n", BARS);
+		return 1;
+	}
+	// use wget command to fetch a wav file from a URL
+	// the following procedure can be put into a forever loop (while(1))
+	FILE *fp = fopen("test.wav", "r");	// open the wav file in read-only
+	WAVheader h = readwavhdr(fp);
+	displaywavhdr(h);
+	getchar();
+	clearscreen();
+	wavdata(h, fp);
+	fclose(fp);
+	getchar();
+}
--- sound.c
+++ sound.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <math.h>
 #include "sound.h"
+#include "screen.h"
 
 // function definitions
 WAVheader readwavhdr(FILE *fp){
@@ -21,3 +23,47 @@ void displaywavhdr(WAVheader h){
 	duration =(double) h.subchunk2Size/h.byteRate;
 	printf("Duration: %f seconds\n", duration);
 }
+
+void wavdata(WAVheader h, FILE *fp){
+	// for sample rate 16000sps, we need to read 2000 samples to calculate a
+	// "Fast" decibel value. A decibel value is always calculated by RMS 
+	// (ROOT MEAN SQUARE) formula
+	short samples[SIZE];
+	int peaks = 0, flag = 0;
+	
+	for(int i=0; i<BARS; i++){	// to read 5-sec wave file, we have 40 data
+		fread(samples, sizeof(samples), 1, fp);
+		double sum = 0.0;
+		for(int k=0; k<SIZE; k++){	// sum the squares of all data
+			sum = sum + samples[k]*samples[k];
+		}
+		//double dB = sqrt(sum/2000);
+		double dB = 20*log10(sqrt(sum/SIZE));
+#ifdef SDEBUG 
+		printf("dB[%d] = %f\n", i, dB);
+#else
+		if(dB > 70) {
+			setfgcolor(RED);
+			if(flag == 0){
+				peaks++; flag=1;
+			}
+		}
+		else {
+			setfgcolor(WHITE);
+			if(flag==1) flag=0;
+		}
+		drawbar(i+1, (int)dB/3);
+		gotoXY(1,1);
+		setfgcolor(CYAN);
+		printf("Sample rate:%d\n", SAMPLERATE);
+		gotoXY(1,75);
+		setfgcolor(MAGENTA);
+		printf("Duration: %.2f\n", (double)h.subchunk2Size/h.byteRate);
+		gotoXY(1,150);
+		setfgcolor(YELLOW);
+		printf("Peaks: %d\n", peaks);
+#endif
+	}
+}
+
+// end of file
